8f0a6a7b1f35d5b500022d22b3d4b9dda1a7f3d4
ratahir <ralitahir@hotmail.com>
2013-12-03 14:40:16 -0500

    updating calculate script for novoalign
    Former-commit-id: ea182c6714835a734d6abd7327037a212cbbb3e8

--- src/test/resources/calculate.sh
+++ src/test/resources/calculate.sh
@@ -16,12 +16,20 @@ sed -i 's/UQ:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDE
 sed -i 's/AS:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
 sed -i 's/PQ:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
 sed -i 's/provisionfiles\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/AM:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/NM:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/SM:\S*/REMOVED/g' SWID_1234_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+
 
 find . -type f -name SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.bam -exec samtools view -h -o SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam {} \;
 sed -i 's/UQ:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
 sed -i 's/AS:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
 sed -i 's/PQ:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
 sed -i 's/provisionfiles\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/AM:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/NM:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+sed -i 's/SM:\S*/REMOVED/g' SWID_1235_library_121005_h804_0096_AD0V4NACXX_NOINDEX_L005_R1_001.annotated.sam;
+
 
 find . -type f -name "*.log" -exec sed -i 's/provisionfiles\S*/tempDirNameRemoved/g' {} \;
 find . -type f -name "*.log" -exec sed -i '/Starting at/d' {} \;
