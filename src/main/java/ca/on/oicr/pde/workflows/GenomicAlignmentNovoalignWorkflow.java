c93bb644274ff76fc0258613cb2f9de4a046a957
Morgan Taschuk <mtaschuk@oicr.on.ca>
2014-05-16 16:42:56 -0400

    PDE-578 Added cutadapt module to Novoalign workflow as optional step. Turned on by default in V3. Also cleaned up the way files are provisioned in and variables are set

--- src/main/java/ca/on/oicr/pde/workflows/GenomicAlignmentNovoalignWorkflow.java
+++ src/main/java/ca/on/oicr/pde/workflows/GenomicAlignmentNovoalignWorkflow.java
@@ -18,14 +18,13 @@ public class GenomicAlignmentNovoalignWorkflow extends OicrWorkflow {
     private String dataDir;
     private String queue;
     private int runEnds;
-    private String [] fastq_inputs_end_1;
-    private String [] fastq_inputs_end_2;
+    private SqwFile [] fastq_inputs_end_1;
+    private SqwFile [] fastq_inputs_end_2;
     private String [] iusAccessions;
     private String [] sequencerRunNames;
     private String [] lanes;
     private String [] barcodes;
     private String [] localOutputBamFilePaths;
-    private String [] localOutputSamFilePaths;
     private String [] localOutputBaiFilePaths;
     private String [] localOutputLogFilePaths;
     private String novocraftVersion ="V2.07.15b" ;
@@ -34,102 +33,32 @@ public class GenomicAlignmentNovoalignWorkflow extends OicrWorkflow {
     
     @Override
     public Map<String, SqwFile> setupFiles() {
-     try {
-         
-         
-       if (getProperty("fastq_inputs_end_1") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "fastq_inputs_end_1 is not set, we need at least one fastq file");
-           return (null);
-       } else {
-           this.fastq_inputs_end_1 = getProperty("fastq_inputs_end_1").split(",");
-       }
-       
-       if (getProperty("fastq_inputs_end_2") == null || getProperty("fastq_inputs_end_2").equals(this.fastq_inputs_end_1)) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.WARNING, "This run will treat the data as single-end reads");
-           this.runEnds = 1;
-       } else {
-           this.fastq_inputs_end_2 = getProperty("fastq_inputs_end_2").split(",");
-           this.runEnds = 2;
-       }
-             
-       if (getProperty("ius_accession") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "ius_accession needs to be set!");
-           return(null);
-       } else {
-           this.iusAccessions = getProperty("ius_accession").split(",");
-       }
-       
-       if (getProperty("sequencer_run_name") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "sequencer_run_name needs to be set!");
-           return(null);
-       } else {
-           this.sequencerRunNames = getProperty("sequencer_run_name").split(",");
-       }
-       
-       if (getProperty("lane") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "lane needs to be set!");
-           return(null);
-       } else {
-           this.lanes = getProperty("lane").split(",");
-       }     
-       
-       if (getProperty("barcode") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "barcode needs to be set!");
-           return(null);
-       } else {
-           this.barcodes = getProperty("barcode").split(",");
-       }
-       
-       this.queue = getOptionalProperty("queue","");         
-          
-       
-       if (getProperty("novocraft_version") == null) {
-           Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.WARNING, "novocraft_version is not set, will use {0}", this.novocraftVersion);
-       } else {
-           this.novocraftVersion = getProperty("novocraft_version");
-       }
-       
-       manualOutput = Boolean.valueOf(getProperty("manual_output"));
-       
-       //Set up inputs
-       for (int fileIndex = 0;fileIndex < this.fastq_inputs_end_1.length; fileIndex++) {         
-          for (int r = 1;r <= this.runEnds; r++) {
-           Log.stdout("CREATING FILE: input_fastq_R" + r + "_" + fileIndex);
-           SqwFile file = this.createFile("input_fastq_R" + r + "_" + fileIndex);
-           switch (r){
-             case 1:
-              file.setSourcePath(this.fastq_inputs_end_1[fileIndex]);
-             break;
-             case 2:
-              file.setSourcePath(this.fastq_inputs_end_2[fileIndex]);
-             break;
-             default:
-              Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, "Something worng, readEnds neither 1 notr 2");
-             break;
-           };
-           file.setType("application/bam");
-           file.setIsInput(true);
-          }
-         }
+        this.runEnds = Integer.valueOf(getProperty("run_ends"));
+        this.iusAccessions = getProperty("ius_accession").split(",");
+        this.sequencerRunNames = getProperty("sequencer_run_name").split(",");
+        this.lanes = getProperty("lane").split(",");
+        this.barcodes = getProperty("barcode").split(",");
+	this.queue = getOptionalProperty("queue","");         
+        this.novocraftVersion = getProperty("novocraft_version");
+       	manualOutput = Boolean.valueOf(getProperty("manual_output"));
+	
+	fastq_inputs_end_1 = provisionInputFiles("fastq_inputs_end_1");
+	fastq_inputs_end_2 = provisionInputFiles("fastq_inputs_end_2"); 
+
        
        //Set up outputs
        localOutputBamFilePaths = new String[this.fastq_inputs_end_1.length];
-       localOutputSamFilePaths = new String[this.fastq_inputs_end_1.length];
        localOutputBaiFilePaths = new String[this.fastq_inputs_end_1.length];
        localOutputLogFilePaths = new String[this.fastq_inputs_end_1.length];
        for (int b = 0; b < localOutputBamFilePaths.length; b++) {
-       localOutputBamFilePaths[b] = "SWID_" + this.iusAccessions[b] + "_" 
-             + getProperty("rg_library") + "_" + this.sequencerRunNames[b] + "_" + this.barcodes[b] 
-             + "_L00" + this.lanes[b] + "_R1_001.annotated.bam";
-       localOutputSamFilePaths[b] = localOutputBamFilePaths[b].substring(0,localOutputBamFilePaths[b].lastIndexOf(".bam")) + ".sam";
-       localOutputBaiFilePaths[b] = localOutputBamFilePaths[b].substring(0,localOutputBamFilePaths[b].lastIndexOf(".bam")) + ".bai";
-       localOutputLogFilePaths[b] = localOutputBamFilePaths[b].substring(0,localOutputBamFilePaths[b].lastIndexOf(".bam")) + ".log";
+       		localOutputBamFilePaths[b] = "SWID_" + this.iusAccessions[b] + "_" 
+             	+ getProperty("rg_library") + "_" + this.sequencerRunNames[b] + "_" + this.barcodes[b] 
+             	+ "_L00" + this.lanes[b] + "_R1_001.annotated.bam";
+       		localOutputBaiFilePaths[b] = localOutputBamFilePaths[b].substring(0,localOutputBamFilePaths[b].lastIndexOf(".bam")) + ".bai";
+       		localOutputLogFilePaths[b] = localOutputBamFilePaths[b].substring(0,localOutputBamFilePaths[b].lastIndexOf(".bam")) + ".log";
        }     
        
-     } catch (Exception e) {
-       Logger.getLogger(GenomicAlignmentNovoalignWorkflow.class.getName()).log(Level.SEVERE, null, e);     
-     }
-     return this.getFiles();    
+	return this.getFiles();    
     }
     
     @Override
@@ -139,41 +68,83 @@ public class GenomicAlignmentNovoalignWorkflow extends OicrWorkflow {
     }
     
     
+    private Job doTrim(String read1Path, String read2Path, String read1TrimmedPath, String read2TrimmedPath) {
+	Job cutAdaptJob = this.getWorkflow().createBashJob("cutAdapt");
+	Command command = cutAdaptJob.getCommand();
+
+	command.addArgument(getWorkflowBaseDir() + "/bin/"+getProperty("bundled_jre")+"/bin/java");
+	command.addArgument("-Xmx500M");
+	command.addArgument("-cp "+getWorkflowBaseDir() + "/classes:"+getWorkflowBaseDir() + "/lib/"+getProperty("bundled_seqware"));
+	command.addArgument("net.sourceforge.seqware.pipeline.runner.PluginRunner -p net.sourceforge.seqware.pipeline.plugins.ModuleRunner -- ");
+	command.addArgument("--module ca.on.oicr.pde.utilities.workflows.modules.CutAdaptModule --no-metadata -- ");
+	command.addArgument("--fastq-read-1 "+read1Path+" --fastq-read-2 "+read2Path);
+	command.addArgument("--output-read-1 "+read1TrimmedPath+" --output-read-2 "+read2TrimmedPath);
+	command.addArgument("--cutadapt \""+getProperty("python")+ " " +getProperty("cutadapt") +"\"");
+	if (!getProperty("trim_min_quality").isEmpty()) command.addArgument("--quality "+getProperty("trim_min_quality"));
+	if (!getProperty("trim_min_length").isEmpty()) command.addArgument("--minimum-length "+getProperty("trim_min_length"));
+	command.addArgument("--adapters-1 "+getProperty("r1_adapter_trim")+" --adapters-2 "+getProperty("r2_adapter_trim"));
+	if (!getProperty("cutadapt_r1_other_params").isEmpty()) command.addArgument("--other-parameters-1 "+getProperty("cutadapt_r1_other_params"));
+	if (!getProperty("cutadapt_r2_other_params").isEmpty()) command.addArgument("--other-parameters-2 "+getProperty("cutadapt_r2_other_params"));
+	cutAdaptJob.setMaxMemory(getProperty("trim_mem_mb"));
+	if (!this.queue.isEmpty()) {
+        	cutAdaptJob.setQueue(this.queue);
+        }
+	return cutAdaptJob;	
+    }
+
+
     @Override
     public void buildWorkflow() {
 	Workflow workflow = this.getWorkflow();
         String[] srn=getProperty("sequencer_run_name").split(","), lanes=getProperty("lane").split(","), barcodes=getProperty("barcode").split(",");
         for (int i = 0; i < this.fastq_inputs_end_1.length; i++) {
+
+		Job trimJob=null;
+		String r1=this.fastq_inputs_end_1[i].getProvisionedPath();
+		String r2=this.fastq_inputs_end_2[i].getProvisionedPath();
+		String basename1 = r1.substring(r1.lastIndexOf("/")+1,r2.lastIndexOf(".fastq.gz"));
+		String basename2 = r2.substring(r2.lastIndexOf("/")+1,r2.lastIndexOf(".fastq.gz"));
+
+		if (Boolean.valueOf(getProperty("do_trim"))) {
+			String trim1=basename1+".trim.fastq.gz";
+			String trim2=basename2+".trim.fastq.gz";
+			trimJob = doTrim(r1,r2,trim1,trim2);
+			r1=trim1;
+			r2=trim2;
+		}
+
 		Job confirmFastq1Job = workflow.createBashJob("confirmFastqFileRead1_"+i);
 		confirmFastq1Job.getCommand().addArgument(getProperty("fastq_validator"));
-		confirmFastq1Job.getCommand().addArgument("--file "+this.fastq_inputs_end_1[i]);
+		confirmFastq1Job.getCommand().addArgument("--file "+r1);
 		confirmFastq1Job.getCommand().addArgument(getProperty("fastq_validator_additional_parameters"));
+		if (trimJob!=null) confirmFastq1Job.addParent(trimJob);
 		if (!this.queue.isEmpty()) {
 			confirmFastq1Job.setQueue(this.queue);
 		}
 
-        	String basename1 = this.fastq_inputs_end_1[i].substring(this.fastq_inputs_end_1[i].lastIndexOf("/")+1);
+        	
 		//The correct format for this is using all underscores, NOT hypens
 		String runlanebarcode=srn[i]+"_"+lanes[i]+"_"+barcodes[i];
 		String readgroup = "-o SAM $'@RG\\tID:"+runlanebarcode+"\\tPU:"+runlanebarcode+"\\tLB:"+getProperty("rg_library")+"\\tSM:"+getProperty("rg_sample_name")+"\\tPL:"+getProperty("rg_platform")+"'";
 
 		Job job_novo=null;
- 		String files = "-f "+getFiles().get("input_fastq_R1_" + i).getProvisionedPath();
-		String adapters=getProperty("novoalign_r1_adapter_trim");
+ 		String files = "-f "+r1;
+		String adapters="-a "+getProperty("r1_adapter_trim");
 		String insert="";
             	if (this.runEnds == 2) {
 			Job confirmFastq2Job = workflow.createBashJob("confirmFastqFileRead2_"+i);
 	                confirmFastq2Job.getCommand().addArgument(getProperty("fastq_validator"));
-        	        confirmFastq2Job.getCommand().addArgument("--file "+this.fastq_inputs_end_2[i]);
+        	        confirmFastq2Job.getCommand().addArgument("--file "+r2);
 			confirmFastq2Job.getCommand().addArgument(getProperty("fastq_validator_additional_parameters"));
 			if (!this.queue.isEmpty()) {
                  	       confirmFastq2Job.setQueue(this.queue);
 	                }
+			if (trimJob!=null) confirmFastq2Job.addParent(trimJob);
 			job_novo = workflow.createBashJob("novoalign_" + i);
 			job_novo.addParent(confirmFastq2Job);
 
-			files+=" " +getFiles().get("input_fastq_R2_" + i).getProvisionedPath();
-			adapters+=" " +getProperty("novoalign_r2_adapter_trim");
+			files+=" " +r2;
+			adapters+=" " +getProperty("r2_adapter_trim");
 			insert= getProperty("novoalign_expected_insert");
 		}
 		else {
