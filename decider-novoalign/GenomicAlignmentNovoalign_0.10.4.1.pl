9115c9624b29e1e5183dd7547ea7ed3064714f4a
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2015-07-03 18:05:12 -0400

    Added licensing to perl and Java files; removed old perl decider

--- decider-novoalign/GenomicAlignmentNovoalign_0.10.4.1.pl
+++ decider-novoalign/GenomicAlignmentNovoalign_0.10.4.1.pl
@@ -3,6 +3,34 @@ use DBI;
 use Data::Dumper;
 use Getopt::Long;
 
+###
+#  Copyright (C) 2015  Ontario Institute of Cancer Research
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Contact us:
+# 
+#  Ontario Institute for Cancer Research  
+#  MaRS Centre, West Tower
+#  661 University Avenue, Suite 510
+#  Toronto, Ontario, Canada M5G 0A3
+#  Phone: 416-977-7599
+#  Toll-free: 1-866-678-6427
+#  www.oicr.on.ca
+###
+
+
 # Author: briandoconnor@gmail.com
 # Desc:   A simple decider that finds all the IUS that correspond to human samples and runs the Novoalign whole genome alignment workflow on them.
 #         It can operate on a flowcell/lane level or on a sample basis.  In each case it looks for candidate IUS that are human, experimental design
